4f29a309f16e92cba5dba351f0c9265c96eb28a1
Johan Malmer <jmalmer@hotmail.com>
2015-10-18 23:39:27 +0200

    Layout #1

--- GetDressed/GetDressed/Form1.Designer.cs
+++ GetDressed/GetDressed/Form1.Designer.cs
@@ -28,20 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TempTrackBar = new System.Windows.Forms.TrackBar();
+            this.HeadPictureBox = new System.Windows.Forms.PictureBox();
+            this.TorsoPictureBox = new System.Windows.Forms.PictureBox();
+            this.LegsPictureBox = new System.Windows.Forms.PictureBox();
+            this.RainTrackBar = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.SettingsPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TempTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeadPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TorsoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LegsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RainTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SettingsPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(8, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(45, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // TempTrackBar
+            // 
+            this.TempTrackBar.Location = new System.Drawing.Point(247, 100);
+            this.TempTrackBar.Maximum = 40;
+            this.TempTrackBar.Minimum = -20;
+            this.TempTrackBar.Name = "TempTrackBar";
+            this.TempTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.TempTrackBar.Size = new System.Drawing.Size(45, 104);
+            this.TempTrackBar.TabIndex = 5;
+            this.TempTrackBar.TickFrequency = 5;
+            this.TempTrackBar.Value = 10;
+            // 
+            // HeadPictureBox
+            // 
+            this.HeadPictureBox.Location = new System.Drawing.Point(84, 77);
+            this.HeadPictureBox.Name = "HeadPictureBox";
+            this.HeadPictureBox.Size = new System.Drawing.Size(136, 67);
+            this.HeadPictureBox.TabIndex = 4;
+            this.HeadPictureBox.TabStop = false;
+            // 
+            // TorsoPictureBox
+            // 
+            this.TorsoPictureBox.Location = new System.Drawing.Point(84, 141);
+            this.TorsoPictureBox.Name = "TorsoPictureBox";
+            this.TorsoPictureBox.Size = new System.Drawing.Size(136, 103);
+            this.TorsoPictureBox.TabIndex = 5;
+            this.TorsoPictureBox.TabStop = false;
+            // 
+            // LegsPictureBox
+            // 
+            this.LegsPictureBox.Location = new System.Drawing.Point(84, 240);
+            this.LegsPictureBox.Name = "LegsPictureBox";
+            this.LegsPictureBox.Size = new System.Drawing.Size(136, 109);
+            this.LegsPictureBox.TabIndex = 6;
+            this.LegsPictureBox.TabStop = false;
+            // 
+            // RainTrackBar
+            // 
+            this.RainTrackBar.Location = new System.Drawing.Point(247, 210);
+            this.RainTrackBar.Name = "RainTrackBar";
+            this.RainTrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.RainTrackBar.Size = new System.Drawing.Size(45, 104);
+            this.RainTrackBar.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(106, 366);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 63);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox5.Location = new System.Drawing.Point(204, 366);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(92, 63);
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(8, 366);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(92, 63);
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            // 
+            // SettingsPictureBox
+            // 
+            this.SettingsPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.SettingsPictureBox.Image = global::GetDressed.Properties.Resources.settings1;
+            this.SettingsPictureBox.Location = new System.Drawing.Point(260, 12);
+            this.SettingsPictureBox.Name = "SettingsPictureBox";
+            this.SettingsPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.SettingsPictureBox.TabIndex = 11;
+            this.SettingsPictureBox.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GetDressed.Properties.Resources.desert;
             this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.SettingsPictureBox);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RainTrackBar);
+            this.Controls.Add(this.LegsPictureBox);
+            this.Controls.Add(this.TorsoPictureBox);
+            this.Controls.Add(this.HeadPictureBox);
+            this.Controls.Add(this.TempTrackBar);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Get Dressed prototype";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TempTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeadPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TorsoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LegsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RainTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SettingsPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar TempTrackBar;
+        private System.Windows.Forms.PictureBox HeadPictureBox;
+        private System.Windows.Forms.PictureBox TorsoPictureBox;
+        private System.Windows.Forms.PictureBox LegsPictureBox;
+        private System.Windows.Forms.TrackBar RainTrackBar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox SettingsPictureBox;
     }
 }
 
