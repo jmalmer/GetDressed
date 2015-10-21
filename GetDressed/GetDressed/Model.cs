bebb0801da876dfd317d5f7360bd41f23bd459d3
Johan Malmer <jmalmer@hotmail.com>
2015-10-21 07:49:45 +0200

    Början till skelett för modell

---
+++ GetDressed/GetDressed/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDressed
+{
+    partial class Model
+    {
+    }
+
+    partial class Person
+    {
+        Garment TorsoGarment { get; set; }
+        Garment LegGarment { get; set; }
+    }
+
+    partial class Garment
+    {
+
+    }
+
+}
